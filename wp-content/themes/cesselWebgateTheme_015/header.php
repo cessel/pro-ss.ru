aec3bdf86a78760572608aec8125428238345e3e
Andrey Ignatov <cessel@yandex.ru>
2022-05-30 15:20:16 +0300

    Autodeploy test

--- wp-content/themes/cesselWebgateTheme_015/header.php
+++ wp-content/themes/cesselWebgateTheme_015/header.php
@@ -15,7 +15,7 @@
     <style>.preloader{top:0;bottom:0;left:0;right:0;position:fixed;display:flex; z-index:99999; align-items:center;align-content:center;justify-content:center;background-color:#fff;font-size:5em;}.preloader i{color:#537791;}body{overflow:hidden;}</style>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title><?php wp_title(); ?></title>
-	<? wp_site_icon(); ?>
+	<?php wp_site_icon(); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
